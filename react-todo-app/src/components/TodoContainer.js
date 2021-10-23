d30d0d95c2c33f329b147bfc5f413bfa048f98a9
mahbub <mahbub21alam2017@gmail.com>
2021-10-23 14:08:00 +0600

    Add class component in TodoContainer file

---
+++ react-todo-app/src/components/TodoContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import { Text, View } from 'react-native'
+
+class TodoContainer extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hello from Create React App</h1>
+                <p>I am in a React Component!</p>
+            </>
+        )
+    }
+}
+
+export default TodoContainer
---
+++ react-todo-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+//component file
+import TodoContainer from './components/TodoContainer'
+
+ReactDom.render(<TodoContainer/>, document.getElementById('root'))
